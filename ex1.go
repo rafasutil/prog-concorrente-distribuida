5b9978c7ccfe45a96b43dcf9bccfd9f704bf4dbe
Rafael Sutil <rsp@cin.ufpe.br>
2022-02-15 11:54:04 -0300

    ex1

---
+++ ex1.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+var M = 10
+
+func main() {
+	var wg sync.WaitGroup
+	m := sync.Mutex{}
+	cond := sync.NewCond(&m)
+
+	wg.Add(1)
+	go pessoa("Rafael", cond)
+	go pessoa("Daniel", cond)
+	go pessoa("Ayrton", cond)
+	go cozinheiro(cond)
+	wg.Wait()
+
+}
+
+func pessoa(nome string, cond *sync.Cond) {
+	for true {
+		// Pegar Porção
+		cond.L.Lock()
+		fmt.Printf("%s vai comer M=%d\n", nome, M)
+		func() {
+			for M == 0 {
+				fmt.Printf("%s aguardando encher M=%d\n", nome, M)
+				cond.Wait()
+			}
+			M--
+			fmt.Printf("%s comeu M=%d\n", nome, M)
+		}()
+		cond.L.Unlock()
+		// Comer
+		time.Sleep(time.Second * 15)
+	}
+}
+
+func cozinheiro(cond *sync.Cond) {
+	for true {
+		time.Sleep(time.Second * 10)
+		// Encher Panela
+		cond.L.Lock()
+		fmt.Printf("Cozinheiro verifica M=%d\n", M)
+		func() {
+			if M == 0 {
+				fmt.Printf("Cozinheiro está enchendo...\n")
+				time.Sleep(time.Second * 5)
+				M = 10
+				cond.Broadcast()
+			}
+		}()
+		cond.L.Unlock()
+	}
+}
